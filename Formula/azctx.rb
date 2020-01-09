a26e8731d6dd27505cd4679f9627a5d694ea878e
Stefan Kürzeder <stefan.kuerzeder@gmail.com>
2020-01-09 19:45:02 +0100

    Update azctx to 0.2

--- Formula/azctx.rb
+++ Formula/azctx.rb
@@ -1,8 +1,8 @@
 class Azctx < Formula
   desc "Switch faster between subscriptions in your azure cli"
   homepage "https://github.com/StiviiK/azctx"
-  url "https://github.com/StiviiK/azctx/releases/download/v0.1/azctx"
-  sha256 "2c62bde8b1c14782d0025952a81175861881eaaa0e8b6b383d934ebca11544e6"
+  url "https://github.com/StiviiK/azctx/releases/download/v0.2/azctx"
+  sha256 "931950371a368d00871885f80450db46f53a7b7023ea70ea3524cd98cd9b16b9"
 
   def install
     bin.install "azctx"
