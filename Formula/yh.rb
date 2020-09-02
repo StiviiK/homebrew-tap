200f95c1b6abceac2dbb34867923357e8b2bd44d
Stefan Kürzeder <stefan.kuerzeder@gmail.com>
2020-09-02 12:34:03 +0200

    Add yh formula

---
+++ Formula/yh.rb
@@ -0,0 +1,10 @@
+class Azctx < Formula
+  desc "A dummy YAML syntax highlighter that brings colours where only jq could."
+  homepage "https://github.com/andreazorzetto/yh"
+  url "https://github.com/andreazorzetto/yh/releases/download/v0.2.1/yh-osx-amd64.zip"
+  sha256 "96cad455d4f33addfba79e2588833635ab6649286cc5988c32eba356a7237c9d"
+
+  def install
+    bin.install "yh"
+  end
+end
